c94f577dd858e5d0179811362213924231e4893b
Yaya Project <117073849+YayaProject@users.noreply.github.com>
2024-08-05 16:18:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,473 @@
+// NILAI RAPOR SEMESTER 1 - 5
+function subtituteData() {
+  const duaApril = [561, 448, 542, 619, 625, 496, 412, 529, 72]
+  const empatApril = [576, 492, 468, 436, 609, 382, 384, 478, 60];
+  const delapanApril = [551, 500, 507, 675, 530, 351, 413, 504, 68];
+  const duaBelasApril = [445, 651, 655, 485, 683, 450, 376, 535, 69]
+  const limaBelasApril = [677, 702, 647, 421, 717, 217, 143, 407, 75];
+  const sembilanBelasApril = [482, 493, 543, 681, 544, 442, 414, 514, 69];
+  const duaPuluhApril = [635, 540, 701, 440, 656, 564, 538, 582, 79];
+  const duaEmpatApril = [510, 463, 512, 448, 530, 353, 382, 457, 59];
+  const duaLimaApril = [524, 416, 628, 485, 609, 458, 424, 506, 64];
+  const duaSembilanApril = [707, 639, 776, 340, 686, 378, 576, 575, 84];
+  
+  document.getElementById("pu-1").innerText = duaApril[0];
+  document.getElementById("ppu-1").innerText = duaApril[1];
+  document.getElementById("pbm-1").innerText = duaApril[2];
+  document.getElementById("pk-1").innerText = duaApril[3];
+  document.getElementById("lbi-1").innerText = duaApril[4];
+  document.getElementById("lbe-1").innerText = duaApril[5];
+  document.getElementById("pm-1").innerText = duaApril[6];
+  document.getElementById("rataRata-1").innerText = duaApril[7];
+
+document.getElementById("pu-2").innerText = empatApril[0];
+document.getElementById("ppu-2").innerText = empatApril[1];
+document.getElementById("pbm-2").innerText = empatApril[2];
+document.getElementById("pk-2").innerText = empatApril[3];
+document.getElementById("lbi-2").innerText = empatApril[4];
+document.getElementById("lbe-2").innerText = empatApril[5];
+document.getElementById("pm-2").innerText = empatApril[6];
+document.getElementById("rataRata-2").innerText = empatApril[7];
+
+document.getElementById("pu-3").innerText = delapanApril[0];
+document.getElementById("ppu-3").innerText = delapanApril[1];
+document.getElementById("pbm-3").innerText = delapanApril[2];
+document.getElementById("pk-3").innerText = delapanApril[3];
+document.getElementById("lbi-3").innerText = delapanApril[4];
+document.getElementById("lbe-3").innerText = delapanApril[5];
+document.getElementById("pm-3").innerText = delapanApril[6];
+document.getElementById("rataRata-3").innerText = delapanApril[7];
+
+document.getElementById("pu-4").innerText = duaBelasApril[0];
+document.getElementById("ppu-4").innerText = duaBelasApril[1];
+document.getElementById("pbm-4").innerText = duaBelasApril[2];
+document.getElementById("pk-4").innerText = duaBelasApril[3];
+document.getElementById("lbi-4").innerText = duaBelasApril[4];
+document.getElementById("lbe-4").innerText = duaBelasApril[5];
+document.getElementById("pm-4").innerText = duaBelasApril[6];
+document.getElementById("rataRata-4").innerText = duaBelasApril[7];
+
+document.getElementById("pu-5").innerText = limaBelasApril[0];
+document.getElementById("ppu-5").innerText = limaBelasApril[1];
+document.getElementById("pbm-5").innerText = limaBelasApril[2];
+document.getElementById("pk-5").innerText = limaBelasApril[3];
+document.getElementById("lbi-5").innerText = limaBelasApril[4];
+document.getElementById("lbe-5").innerText = limaBelasApril[5];
+document.getElementById("pm-5").innerText = limaBelasApril[6];
+document.getElementById("rataRata-5").innerText = limaBelasApril[7];
+
+document.getElementById("pu-6").innerText = sembilanBelasApril[0];
+document.getElementById("ppu-6").innerText = sembilanBelasApril[1];
+document.getElementById("pbm-6").innerText = sembilanBelasApril[2];
+document.getElementById("pk-6").innerText = sembilanBelasApril[3];
+document.getElementById("lbi-6").innerText = sembilanBelasApril[4];
+document.getElementById("lbe-6").innerText = sembilanBelasApril[5];
+document.getElementById("pm-6").innerText = sembilanBelasApril[6];
+document.getElementById("rataRata-6").innerText = sembilanBelasApril[7];
+
+document.getElementById("pu-7").innerText = duaPuluhApril[0];
+document.getElementById("ppu-7").innerText = duaPuluhApril[1];
+document.getElementById("pbm-7").innerText = duaPuluhApril[2];
+document.getElementById("pk-7").innerText = duaPuluhApril[3];
+document.getElementById("lbi-7").innerText = duaPuluhApril[4];
+document.getElementById("lbe-7").innerText = duaPuluhApril[5];
+document.getElementById("pm-7").innerText = duaPuluhApril[6];
+document.getElementById("rataRata-7").innerText = duaPuluhApril[7];
+
+document.getElementById("pu-8").innerText = duaEmpatApril[0];
+document.getElementById("ppu-8").innerText = duaEmpatApril[1];
+document.getElementById("pbm-8").innerText = duaEmpatApril[2];
+document.getElementById("pk-8").innerText = duaEmpatApril[3];
+document.getElementById("lbi-8").innerText = duaEmpatApril[4];
+document.getElementById("lbe-8").innerText = duaEmpatApril[5];
+document.getElementById("pm-8").innerText = duaEmpatApril[6];
+document.getElementById("rataRata-8").innerText = duaEmpatApril[7];
+
+document.getElementById("pu-9").innerText = duaLimaApril[0];
+document.getElementById("ppu-9").innerText = duaLimaApril[1];
+document.getElementById("pbm-9").innerText = duaLimaApril[2];
+document.getElementById("pk-9").innerText = duaLimaApril[3];
+document.getElementById("lbi-9").innerText = duaLimaApril[4];
+document.getElementById("lbe-9").innerText = duaLimaApril[5];
+document.getElementById("pm-9").innerText = duaLimaApril[6];
+document.getElementById("rataRata-9").innerText = duaLimaApril[7];
+
+document.getElementById("pu-10").innerText = duaSembilanApril[0];
+document.getElementById("ppu-10").innerText = duaSembilanApril[1];
+document.getElementById("pbm-10").innerText = duaSembilanApril[2];
+document.getElementById("pk-10").innerText = duaSembilanApril[3];
+document.getElementById("lbi-10").innerText = duaSembilanApril[4];
+document.getElementById("lbe-10").innerText = duaSembilanApril[5];
+document.getElementById("pm-10").innerText = duaSembilanApril[6];
+document.getElementById("rataRata-10").innerText = duaSembilanApril[7];
+
+  const a = 10;
+  
+const rataRataPU = (
+  duaApril[0] + 
+  empatApril[0] + 
+  delapanApril[0] +
+  duaBelasApril[0] +
+  limaBelasApril[0] +
+  sembilanBelasApril[0] +
+  duaPuluhApril[0] +
+  duaEmpatApril[0] +
+  duaLimaApril[0] +
+  duaSembilanApril[0]) / a;
+  
+const rataRataPPU = (
+  duaApril[1] + 
+  empatApril[1] + 
+  delapanApril[1] +
+  duaBelasApril[1] +
+  limaBelasApril[1] +
+  sembilanBelasApril[1] +
+  duaPuluhApril[1] +
+  duaEmpatApril[1] +
+  duaLimaApril[1] +
+  duaSembilanApril[1]) / a;
+  
+const rataRataPBM = (
+  duaApril[2] + 
+  empatApril[2] + 
+  delapanApril[2] +
+  duaBelasApril[2] +
+  limaBelasApril[2] +
+  sembilanBelasApril[2] +
+  duaPuluhApril[2] +
+  duaEmpatApril[2] +
+  duaLimaApril[2] +
+  duaSembilanApril[2]) / a;
+  
+const rataRataPK = (
+  duaApril[3] + 
+  empatApril[3] + 
+  delapanApril[3] +
+  duaBelasApril[3] +
+  limaBelasApril[3] +
+  sembilanBelasApril[3] +
+  duaPuluhApril[3] +
+  duaEmpatApril[3] +
+  duaLimaApril[3] +
+  duaSembilanApril[3]) / a;
+  
+const rataRataLBI = (
+  duaApril[4] + 
+  empatApril[4] + 
+  delapanApril[4] +
+  duaBelasApril[4] +
+  limaBelasApril[4] +
+  sembilanBelasApril[4] +
+  duaPuluhApril[4] +
+  duaEmpatApril[4] +
+  duaLimaApril[4] +
+  duaSembilanApril[4]) / a;
+  
+const rataRataLBE = (
+  duaApril[5] + 
+  empatApril[5] + 
+  delapanApril[5] +
+  duaBelasApril[5] +
+  limaBelasApril[5] +
+  sembilanBelasApril[5] +
+  duaPuluhApril[5] +
+  duaEmpatApril[5] +
+  duaLimaApril[5] +
+  duaSembilanApril[5]) / a;
+  
+const rataRataPM = (
+  duaApril[6] + 
+  empatApril[6] + 
+  delapanApril[6] +
+  duaBelasApril[6] +
+  limaBelasApril[6] +
+  sembilanBelasApril[6] +
+  duaPuluhApril[6] +
+  duaEmpatApril[6] +
+  duaLimaApril[6] +
+  duaSembilanApril[6]) / a;
+
+const totalRataRata = (
+  duaApril[7] + 
+  empatApril[7] + 
+  delapanApril[7] +
+  duaBelasApril[7] +
+  limaBelasApril[7] +
+  sembilanBelasApril[7] +
+  duaPuluhApril[7] +
+  duaEmpatApril[7] +
+  duaLimaApril[7] +
+  duaSembilanApril[7]) / a;
+  
+const rataRataSoal = (
+  duaApril[8] + 
+  empatApril[8] + 
+  delapanApril[8] +
+  duaBelasApril[8] +
+  limaBelasApril[8] +
+  sembilanBelasApril[8] +
+  duaPuluhApril[8] +
+  duaEmpatApril[8] +
+  duaLimaApril[8] +
+  duaSembilanApril[8]) / a;
+
+  // GRAFIK RATA-RATA HASIL TRY OUT
+  Chart.defaults.color = "black";
+  Chart.defaults.font.family = "Poppins, sans-serif";
+let delayed;
+
+  const ctx1 = document.getElementById("myChart1");
+  const rataRata = [
+    duaApril[7], 
+    empatApril[7], 
+    delapanApril[7],
+    duaBelasApril[7],
+    limaBelasApril[7],
+    sembilanBelasApril[7],
+    duaPuluhApril[7],
+    duaEmpatApril[7],
+    duaLimaApril[7],
+    duaSembilanApril[7],
+  ]; 
+  new Chart(ctx1, {
+    type: "line",
+    data: {
+      labels: [
+        "2 April",
+        "4 April",
+        "8 April",
+        "12 April",
+        "15 April",
+        "19 April",
+        "20 April",
+        "24 April",
+        "25 April",
+        "29 April",
+      ],
+      datasets: [
+        {
+          label: "Rata-Rata",
+          data: rataRata,
+          hoverBackgroundColor: "rgba(30, 144, 255, 0.5)",
+          backgroundColor: "#1e90ff",
+          borderColor: "#1e90ff",
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      animation: {
+        onComplete: () => {
+          delayed = true;
+        },
+        delay: (context) => {
+          let delay = 0;
+          if (
+            context.type === "data" &&
+            context.mode === "default" &&
+            !delayed
+          ) {
+            delay = context.dataIndex * 300 + context.datasetIndex * 100;
+          }
+          return delay;
+        },
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: "Grafik Rata-Rata Hasil Try Out",
+          font: {
+            size: 15,
+          },
+        },
+        subtitle: {
+          display: true,
+          text: "Rata-Rata : " + totalRataRata.toFixed(0),
+          font: {
+            size: 12,
+          },
+          padding: {
+            bottom: 15,
+          },
+        },
+        legend: {
+          display: false,
+        },
+      },
+      scales: {
+        y: {
+          min: 300,
+          max: 700,
+        },
+      },
+    },
+  });
+  
+  const ctx2 = document.getElementById("myChart2");
+  const jumlahBenar = [
+    duaApril[8], 
+    empatApril[8], 
+    delapanApril[8],
+    duaBelasApril[8],
+    limaBelasApril[8],
+    sembilanBelasApril[8],
+    duaPuluhApril[8],
+    duaEmpatApril[8],
+    duaLimaApril[8],
+    duaSembilanApril[8],
+  ]; 
+  new Chart(ctx2, {
+    type: "bar",
+    data: {
+      labels: [
+        "2 April",
+        "4 April",
+        "8 April",
+        "12 April",
+        "15 April",
+        "19 April",
+        "20 April",
+        "24 April",
+        "25 April",
+        "29 April",
+      ],
+      datasets: [
+        {
+          label: "Jumlah Benar",
+          data: jumlahBenar,
+          hoverBackgroundColor: "#1e90ff",
+          backgroundColor: "rgba(30, 144, 255, 0.2)",
+          borderColor: "#1e90ff",
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      animation: {
+        onComplete: () => {
+          delayed = true;
+        },
+        delay: (context) => {
+          let delay = 0;
+          if (
+            context.type === "data" &&
+            context.mode === "default" &&
+            !delayed
+          ) {
+            delay = context.dataIndex * 300 + context.datasetIndex * 100;
+          }
+          return delay;
+        },
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: "Grafik Jumlah Benar Per Try Out",
+          font: {
+            size: 15,
+          },
+        },
+        subtitle: {
+          display: true,
+          text: "Rata-Rata : " + rataRataSoal.toFixed(0),
+          font: {
+            size: 12,
+          },
+          padding: {
+            bottom: 15,
+          },
+        },
+        legend: {
+          display: false,
+        },
+      },
+      scales: {
+        y: {
+          min: 50,
+          max: 100,
+        },
+      },
+    },
+  });
+  
+    // GRAFIK KENAIKAN NILAI TIAP SEMESTER
+  const ctx3 = document.getElementById("myChart3");
+
+  new Chart(ctx3, {
+    type: "radar",
+    data: {
+      labels: [
+        "PU",
+        "PPU",
+        "PBM",
+        "PK",
+        "LBI",
+        "LBE",
+        "PM",
+      ],
+      datasets: [
+        {
+          label: "Nilai",
+          data: [
+            rataRataPU.toFixed(0),
+            rataRataPPU.toFixed(0),
+            rataRataPBM.toFixed(0),
+            rataRataPK.toFixed(0),
+            rataRataLBI.toFixed(0),
+            rataRataLBE.toFixed(0),
+            rataRataPM.toFixed(0),
+          ],
+          backgroundColor: "rgba(30, 144, 255, 0.2)",
+          borderColor: "#1e90ff",
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      animation: {
+        onComplete: () => {
+          delayed = true;
+        },
+        delay: (context) => {
+          let delay = 0;
+          if (
+            context.type === "data" &&
+            context.mode === "default" &&
+            !delayed
+          ) {
+            delay = context.dataIndex * 300 + context.datasetIndex * 100;
+          }
+          return delay;
+        },
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: "Grafik Rata-Rata Tiap Subtes",
+          font: {
+            size: 15,
+          },
+        },
+        legend: {
+          display: false,
+        },
+      },
+      scales: {
+        r: {
+          min: 100,
+          max: 700,
+        },
+      },
+    },
+  });
+  
+}
+document.addEventListener("DOMContentLoaded", function () {
+  subtituteData();
+});
